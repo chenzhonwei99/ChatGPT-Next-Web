dfe9775d84335106b5ad4bdfb2b04b7a9e4e1804
chenzhonwei99 <104198155+chenzhonwei99@users.noreply.github.com>
2023-08-02 20:03:58 +0800

    Update global.d.ts

--- app/global.d.ts
+++ app/global.d.ts
@@ -10,6 +10,10 @@ declare module "*.scss" {
 
 declare module "*.svg";
 
+interface Window {
+  adsbygoogle: { [key: string]: unknown }[];
+}
+
 declare interface Window {
   __TAURI__?: {
     writeText(text: string): Promise<void>;
