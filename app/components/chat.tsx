5824631aee10de90a546cff44eaeffb4d18055d4
chenzhonwei99 <104198155+chenzhonwei99@users.noreply.github.com>
2023-08-03 22:53:41 +0800

    Update chat.tsx

--- app/components/chat.tsx
+++ app/components/chat.tsx
@@ -1178,15 +1178,8 @@ export function Chat() {
       </div>
 
 
-  export function SideBar(props: { className?: string }) {
-  const chatStore = useChatStore();
-
-  // drag side bar
-  const { onDragMouseDown, shouldNarrow } = useDragSideBar();
-  const navigate = useNavigate();
-  const config = useAppConfig();
-
-  useHotKey();
+export function YourComponent() {
+  const adRef = useRef(null);
 
   useEffect(() => {
     const script = document.createElement("script");
@@ -1200,53 +1193,53 @@ export function Chat() {
     };
   }, []);
 
-  const adRef = useRef(null);
-
-useEffect(() => {
-  if (adRef.current) {
-    (window.adsbygoogle = window.adsbygoogle || []).push({});
-  }
-}, [adRef]);
-
-useEffect(() => {
-  if (!adRef.current) {
-    return;
-  }
-
-  const observer = new MutationObserver(() => {
-    if (adRef.current && (adRef.current as HTMLElement).querySelector("iframe")) {
-      (adRef.current as HTMLElement).style.width = "250px";
-      (adRef.current as HTMLElement).style.height = "208px";
+  useEffect(() => {
+    if (adRef.current) {
+      (window.adsbygoogle = window.adsbygoogle || []).push({});
     }
-  });
+  }, [adRef]);
 
-  observer.observe(adRef.current, { childList: true, subtree: true });
+  useEffect(() => {
+    if (!adRef.current) {
+      return;
+    }
 
-  return () => {
-    observer.disconnect();
-  };
-}, []);
+    const observer = new MutationObserver(() => {
+      if (adRef.current && (adRef.current as HTMLElement).querySelector("iframe")) {
+        (adRef.current as HTMLElement).style.width = "250px";
+        (adRef.current as HTMLElement).style.height = "208px";
+      }
+    });
 
-return (
-<div
-  style={{
-    minWidth: "250px",
-    minHeight: "208px",
-    overflow: "hidden", // 隐藏超出容器的内容
-    display: "flex",
-    justifyContent: "center",
-    marginBottom: "20px"
-  }}
->
-  <ins
-    ref={adRef}
-    className="adsbygoogle"
-    style={{ display: "block", minWidth: "250px", minHeight: "208px" }}
-    data-ad-client="ca-pub-1027588722085336"
-    data-ad-slot="5771884005"
-    data-full-width-responsive="true"
-  />
-</div>
+    observer.observe(adRef.current, { childList: true, subtree: true });
+
+    return () => {
+      observer.disconnect();
+    };
+  }, []);
+
+  return (
+    <div
+      style={{
+        minWidth: "250px",
+        minHeight: "208px",
+        overflow: "hidden", // 隐藏超出容器的内容
+        display: "flex",
+        justifyContent: "center",
+        marginBottom: "20px"
+      }}
+    >
+      <ins
+        ref={adRef}
+        className="adsbygoogle"
+        style={{ display: "block", minWidth: "250px", minHeight: "208px" }}
+        data-ad-client="ca-pub-1027588722085336"
+        data-ad-slot="5771884005"
+        data-full-width-responsive="true"
+      />
+    </div>
+  );
+}
       
       
       <div className={styles["chat-input-panel"]}>
