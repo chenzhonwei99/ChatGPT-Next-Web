2f8e28fbfc7111007daba6e799ed2292dd580c9e
chenzhonwei99 <104198155+chenzhonwei99@users.noreply.github.com>
2023-08-03 23:27:38 +0800

    Update chat.tsx

--- app/components/chat.tsx
+++ app/components/chat.tsx
@@ -1178,68 +1178,6 @@ export function Chat() {
       </div>
 
 
-import React, { useEffect, useRef } from 'react';
-
-export function AdsComponent() {
-  const adRef = useRef(null);
-
-  useEffect(() => {
-    const script = document.createElement("script");
-    script.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1027588722085336";
-    script.async = true;
-    script.crossOrigin = "anonymous";
-    document.head.appendChild(script);
-
-    return () => {
-      document.head.removeChild(script);
-    };
-  }, []);
-
-  useEffect(() => {
-    if (adRef.current) {
-      (window.adsbygoogle = window.adsbygoogle || []).push({});
-    }
-  }, []);
-
-  useEffect(() => {
-    if (!adRef.current) {
-      return;
-    }
-
-    const observer = new MutationObserver(() => {
-      if (adRef.current && (adRef.current as HTMLElement).querySelector("iframe")) {
-        (adRef.current as HTMLElement).style.width = "250px";
-        (adRef.current as HTMLElement).style.height = "208px";
-      }
-    });
-
-    observer.observe(adRef.current, { childList: true, subtree: true });
-
-    return () => {
-      observer.disconnect();
-    };
-  }, []);
-
-  return (
-    <div
-      style={{
-        minWidth: "250px",
-        minHeight: "208px",
-        overflow: "hidden", // 隐藏超出容器的内容
-        display: "flex",
-        justifyContent: "center",
-        marginBottom: "20px"
-      }}
-    >
-      <ins
-        ref={adRef}
-        className="adsbygoogle"
-        style={{ display: "block", minWidth: "250px", minHeight: "208px" }}
-        data-ad-client="ca-pub-1027588722085336"
-        data-ad-slot="5771884005"
-        data-full-width-responsive="true"
-      />
-    </div>
       
       
       <div className={styles["chat-input-panel"]}>
